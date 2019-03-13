e320ca976bd546eb0096b2cd4f7f8ae6943b6212
lizhichange@163.com <lizhi1234>
2019-03-13 15:52:33 +0800

    '1'

--- duoduoke-service/src/main/java/com/fulihui/duoduoke/demo/producer/ProducerBootstrap.java
+++ duoduoke-service/src/main/java/com/fulihui/duoduoke/demo/producer/ProducerBootstrap.java
@@ -3,13 +3,13 @@ package com.fulihui.duoduoke.demo.producer;
 import org.mybatis.spring.annotation.MapperScan;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.context.annotation.ComponentScan;
 
-@EnableAutoConfiguration
 @MapperScan(basePackages = {"com.fulihui.duoduoke.demo.producer.dal.dao"})
 @ComponentScan("com.fulihui.duoduoke.demo")
+@SpringBootApplication
 public class ProducerBootstrap {
 
     private final Logger logger = LoggerFactory.getLogger(getClass());
--- duoduoke-web/src/main/java/com/fulihui/duoduoke/demo/web/ConsumerBootstrap.java
+++ duoduoke-web/src/main/java/com/fulihui/duoduoke/demo/web/ConsumerBootstrap.java
@@ -7,12 +7,13 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 /**
  * @author wahaha
  */
-@EnableAutoConfiguration
+@SpringBootApplication
 @ComponentScan("com.fulihui.duoduoke.demo")
 public class ConsumerBootstrap {
     private final Logger logger = LoggerFactory.getLogger(getClass());
