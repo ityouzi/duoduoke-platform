a1d58e134cf692fbeed149a597537225375314ff
lizhichange@163.com <lizhi1234>
2019-03-13 17:12:10 +0800

    '1'

--- duoduoke-service/src/main/java/com/fulihui/duoduoke/demo/producer/ProducerBootstrap.java
+++ duoduoke-service/src/main/java/com/fulihui/duoduoke/demo/producer/ProducerBootstrap.java
@@ -4,12 +4,13 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.context.annotation.ComponentScan;
 
 @MapperScan(basePackages = {"com.fulihui.duoduoke.demo.producer.dal.dao"})
 @ComponentScan("com.fulihui.duoduoke.demo")
-@EnableAutoConfiguration
+@SpringBootApplication
 public class ProducerBootstrap {
 
     private final Logger logger = LoggerFactory.getLogger(getClass());
