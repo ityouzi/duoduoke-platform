e2c84573f5b1ae9b9af137445ea167fa2424db62
lizhichange@163.com <lizhi1234>
2019-03-13 16:05:25 +0800

    '1'

--- duoduoke-web/src/main/java/com/fulihui/duoduoke/demo/web/ConsumerBootstrap.java
+++ duoduoke-web/src/main/java/com/fulihui/duoduoke/demo/web/ConsumerBootstrap.java
@@ -7,13 +7,12 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 /**
  * @author wahaha
  */
-@SpringBootApplication
+@EnableAutoConfiguration
 @ComponentScan("com.fulihui.duoduoke.demo")
 public class ConsumerBootstrap {
     private final Logger logger = LoggerFactory.getLogger(getClass());
