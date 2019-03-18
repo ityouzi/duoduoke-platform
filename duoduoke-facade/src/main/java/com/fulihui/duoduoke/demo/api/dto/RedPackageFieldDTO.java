1af92bf93175e0cfe3b78bdda45bdd327af62294
lizhichange@163.com <lizhi1234>
2019-03-18 14:46:41 +0800

    '1'

--- duoduoke-facade/src/main/java/com/fulihui/duoduoke/demo/api/dto/RedPackageFieldDTO.java
+++ duoduoke-facade/src/main/java/com/fulihui/duoduoke/demo/api/dto/RedPackageFieldDTO.java
@@ -1,6 +1,7 @@
 package com.fulihui.duoduoke.demo.api.dto;
 
 import lombok.Data;
+import org.near.toolkit.model.ToString;
 
 import java.util.Date;
 
@@ -9,7 +10,7 @@ import java.util.Date;
  * @date: 2018/9/3 15:28
  */
 @Data
-public class RedPackageFieldDTO {
+public class RedPackageFieldDTO extends ToString {
 
     /**
      * 主键
@@ -27,7 +28,7 @@ public class RedPackageFieldDTO {
     private Short state;
 
     /**
-     *专场类型[1:红包专场][2:无助力红包专场]
+     * 专场类型[1:红包专场][2:无助力红包专场]
      */
     private Integer type;
 
